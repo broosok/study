307ab996963d7d326f781b68d3e9168e7c60d1af
broosok <dukker010@gmail.com>
2025-10-05 13:18:48 +0300

    Пофикшен баг с фикстурой

--- tests/test_button_page.py
+++ tests/test_button_page.py
@@ -1,5 +1,13 @@
 from qa_practice.pages.simple_button import SimpleButtonPage
+import pytest
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 
+@pytest.fixture()
+def browser():
+    firefox_browser = webdriver.Firefox()
+    firefox_browser.implicitly_wait(10)
+    return firefox_browser
 
 def test_button1_exist(browser):
     simple_page = SimpleButtonPage(browser)
