925a371dd4592e27083b9cda7257f1916c2d2411
broosok <dukker010@gmail.com>
2025-10-12 16:11:04 +0300

    some fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,6 @@ from selenium import webdriver
 def browser():
     options = Options()
     options.add_argument('--headless')
-    chrome_browser = webdriver.Chrome(options=options)
-    chrome_browser.implicitly_wait(10)
-    return chrome_browser
+    firefox_browser = webdriver.Firefox(options=options)
+    firefox_browser.implicitly_wait(10)
+    return firefox_browser
